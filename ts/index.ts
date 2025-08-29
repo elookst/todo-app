ca63685965bfbb11c272fb8e340bb6187b7b1d60
elookst <eluksts@gmail.com>
2025-08-28 23:39:23 -0400

    added checkbox button to set tasks to done

--- ts/index.ts
+++ ts/index.ts
@@ -1,6 +1,7 @@
 interface Task {
     id: number;
     text: string;
+    isDone: boolean;
 }
 
 const inputs = document.querySelector('input') as HTMLInputElement;
@@ -30,7 +31,8 @@ btn.addEventListener("click", function ()
 
     const taskObj: Task = {
         id: Date.now(),
-        text: query
+        text: query,
+        isDone: false
     };
 
     // add new task to the array
@@ -45,25 +47,30 @@ function maketodo(): void
 
     for (let i = 0; i < task.length; i++ )
     {
-        const { id, text } = task[i];
+        const { id, text, isDone } = task[i];
         const element = document.createElement('div');
         element.innerHTML = `
             <span class="task">${text}</span>
-            <span class="delete">
-                <svg xmlns="https://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                    <path d="M17 6H22V8H20V21C20 21.5523 
-                    19.5523 22 19 22H5C4.44772 22 4 21.5523
-                     4 21V8H2V6H7V3C7 2.44772 7.44772 2 8 
-                     2H16C16.5523 2 17 2.44772 17 3V6ZM18 
-                     8H6V20H18V8ZM13.4142 13.9997L15.182 
-                     15.7675L13.7678 17.1817L12 15.4139L10.2322
-                      17.1817L8.81802 15.7675L10.5858 13.9997L8.81802
-                       12.232L10.2322 10.8178L12 12.5855L13.7678
-                        10.8178L15.182 12.232L13.4142 13.9997ZM9
-                         4V6H15V4H9Z"></path>
-                </svg>
+            <span class="isDone">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
+  <path d="M9 16.2l-3.5-3.5-1.4 1.4L9 19 20 8l-1.4-1.4z"/>
+</svg>
             </span>
+            <span class="delete">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
+  <path d="M6 7h12v12c0 1.1-.9 2-2 2H8c-1.1 0-2-.9-2-2V7zm3-4h6v2H9V3zM4 7h16v2H4V7z"/>
+</svg>
+
+                </span>
         `;
+
+        // const setDoneBtn = element.querySelector('.isDone')!;
+        // setDoneBtn.addEventListener("click", function ()
+        // {
+        //     const filteredTasks = task.filter((taskObj: Task) => taskObj.id !== id);
+        //     task = filteredTasks;
+        //     localStorage.setItem("task array", JSON.stringify(task));
+        // })
 
         const delBtn = element.querySelector('.delete')!;
         delBtn.addEventListener("click", function ()
